348111da60d15435e05fb16099df48513f340bf6
Travis Exline <t.exline22@gmail.com>
2019-08-07 18:11:56 +0000

    views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'application#index'
-  post '/signin' => 'session#create'
-  get '/signin' => 'session#new'
-  get '/logout' => 'session#destroy'
+  post '/signin' => 'sessions#create'
+  get '/signin' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
 
   resources :posts
   resources :users
